12b66dd83544d19dee9686dcfb6dc968574ea092
GuruF <guruprasath@aidaptive.com>
2024-04-29 22:30:16 +0530

    Removed Icons

--- components/pages/Categories.tsx
+++ components/pages/Categories.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 // Dummy category data
 const categories = [
-  { id: 1, name: 'Body wash', icon: '🚿' },
-  { id: 2, name: 'Serum', icon: '💧' },
-  { id: 3, name: 'Facewash', icon: '😊' },
-  { id: 4, name: 'Moisturizer', icon: '💦' },
-  { id: 5, name: 'Sunscreen', icon: '☀️' },
-  { id: 6, name: 'Toner', icon: '🌹' },
+  { id: 1, name: 'Body wash',  },
+  { id: 2, name: 'Serum',  },
+  { id: 3, name: 'Facewash',  },
+  { id: 4, name: 'Moisturizer',  },
+  { id: 5, name: 'Sunscreen',  },
+  { id: 6, name: 'Toner', },
 ];
 
 function Categories() {
@@ -17,7 +17,6 @@ function Categories() {
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {categories.map(category => (
           <div key={category.id} className="bg-white p-4 shadow rounded-lg flex items-center">
-            <span className="text-4xl mr-4">{category.icon}</span>
             <span className="text-lg font-bold">{category.name}</span>
           </div>
         ))}
