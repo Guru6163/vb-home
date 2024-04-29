6d39335fef3b9f64833f573eab5566f78583fb8d
GuruF <guruprasath@aidaptive.com>
2024-04-29 23:14:35 +0530

    Products

--- components/pages/Products.tsx
+++ components/pages/Products.tsx
@@ -1,29 +1,6 @@
 import React from 'react';
+import { products } from '@/constants/Products';
 
-// Dummy product data
-const products = [
-  {
-    id: 1,
-    name: 'Product 1',
-    description: 'This is the description for product 1.',
-    price: '$10.99',
-    imageUrl: 'https://via.placeholder.com/150',
-  },
-  {
-    id: 2,
-    name: 'Product 2',
-    description: 'This is the description for product 2.',
-    price: '$20.99',
-    imageUrl: 'https://via.placeholder.com/150',
-  },
-  {
-    id: 3,
-    name: 'Product 3',
-    description: 'This is the description for product 3.',
-    price: '$30.99',
-    imageUrl: 'https://via.placeholder.com/150',
-  },
-];
 
 function Products() {
   return (
@@ -32,10 +9,10 @@ function Products() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map(product => (
           <div key={product.id} className="bg-white p-4 shadow rounded-lg">
-            <img src={product.imageUrl} alt={product.name} className="w-full h-40 object-cover mb-4" />
-            <h2 className="text-lg font-bold mb-2">{product.name}</h2>
-            <p className="text-gray-600 mb-2">{product.description}</p>
-            <p className="text-green-600 font-bold">{product.price}</p>
+            <img src={product.imageUrl} alt={product.productName} className="w-full h-80 object-cover mb-4 rounded-md" />
+            <h2 className="text-lg font-bold mb-2">{product.productName}</h2>
+            <p className="text-gray-600 mb-2">{product.category}</p>
+            <p className="text-green-600 font-bold">{product.sellingCost}</p>
           </div>
         ))}
       </div>
