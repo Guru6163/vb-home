cb038a3a32fff3915477649f42f53a6dda0174ee
GuruF <guruprasath@aidaptive.com>
2024-04-29 23:15:15 +0530

    Dummy

--- components/pages/Products.tsx
+++ components/pages/Products.tsx
@@ -9,7 +9,7 @@ function Products() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map(product => (
           <div key={product.id} className="bg-white p-4 shadow rounded-lg">
-            <img src={product.imageUrl} alt={product.productName} className="w-full h-80 object-cover mb-4 rounded-md" />
+            <img src={'https://res.cloudinary.com/dua8sce9b/image/upload/v1714412328/Introducing_Neem-Tulasi_Your_Ultimate_Solution_to_Dandruff_Woes_ziepkv.svg'} alt={product.productName} className="w-full h-80 object-cover mb-4 rounded-md" />
             <h2 className="text-lg font-bold mb-2">{product.productName}</h2>
             <p className="text-gray-600 mb-2">{product.category}</p>
             <p className="text-green-600 font-bold">{product.sellingCost}</p>
